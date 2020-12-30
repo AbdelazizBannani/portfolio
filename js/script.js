3f2c3f334e40224fc3c97e2a8209b091dc862b46
abdelaziz bannani <abdelaziz.bannani@ttl,be>
2020-12-30 16:24:10 +0100

    contact section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+
+$(window).on("load", function() 
+{
+$(".loader .inner").fadeOut(500, function()
+    {
+    $(".loader").fadeOut(750);
+    });
+
+    $(".items").isotope(
+        {
+        filter: '*',
+        layoutMode: 'fitRows',
+        animationOptions:
+            {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+            }
+        });
+});
+
+
+
+
+
 $(document).ready(function()  {
     $('#slides').superslides({
             animation: 'fade',
@@ -6,99 +31,101 @@ $(document).ready(function()  {
 });
 
 
-// var typed = new Typed(".typed", {
-//     strings: ["Software Engineer", "QA developer", "Test Automation engineer", "Super Daddy"], 
-//     typeSpeed: 70,
-//     loop:true,
-//     startDelay:1000,
-//     showCursor:true
-// });
+
+
+var typed = new Typed(".typed", {
+    strings: ["Software Engineer", "QA developer", "Test Automation engineer", "Super Daddy"], 
+    typeSpeed: 70,
+    loop:true,
+    startDelay:1000,
+    showCursor:true
+});
 
 
 
 
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     items: 8,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         200:{
-//             items:4
-//         },
-//         768:{
-//             items:5
-//         },
-//         1080:{
-//             items:7
-//         }
-//     }
-// });
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 8,
+    responsive:{
+        0:{
+            items:1
+        },
+        200:{
+            items:4
+        },
+        768:{
+            items:5
+        },
+        1080:{
+            items:7
+        }
+    }
+});
 
 
 
 
 
-// var skillsTopOffset = $(".skillsSection").offset().top;
-// var skillsTopOffset = $(".statsSection").offset().top;
 
-// console.log(window.pageYOffset);
 
-// $(window).scroll(function() {
 
-//     if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+var skillsTopOffset = $(".skillsSection").offset().top;
+var statsTopOffset = $(".statsSection").offset().top;
 
-//         $('.chart').easyPieChart({
-//             //your options goes here
-//             easing:'easeInOut',
-//             barColor: '#C4E538',
-//             trackColor: false,
-//             scaleColor: false,
-//             lineWidth: 4, 
-//             size:152,
-//             onStep: function(from, to, percent) {
-//                 $(this.el).find('.percent').text(Math.round(percent));
-//             }
-//         });
+$(window).scroll(function() {
+    if(window.pageYOffset > skillsTopOffset - $(window).height() + 200)
+        {
+        $('.chart').easyPieChart(
+            {
+            easing: 'easeInOut',
+            barColor: '#fff',
+            trackColor: false,
+            scaleColor: false,
+            lineWidth: 4,
+            size: 152,
+            onStep: function(from, to, percent) 
+                {
+                $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
 
-//     }
-
-//     if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-
-//         $('.counter').each(function() {
-//             var $this = $(this),
-//                 countTo = $this.attr('data-count');
+    if(window.pageYOffset > statsTopOffset - $(window).height() + 200)
+        {
+            $('.counter').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
             
-//             $({ countNum: $this.text()}).animate({
-//                 countNum: countTo
-//             },
+            $({ countNum: $this.text()}).animate({
+                countNum: countTo
+            },
         
-//             {
+            {
+                duration: 2000,
+                easing:'linear',
+                step: function() {
+                $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                $this.text(this.countNum);
+                //alert('finished');
+                }
             
-//                 duration: 1000,
-//                 easing:'linear',
-//                 step: function() {
-//                 $this.text(Math.floor(this.countNum));
-//                 },
-//                 complete: function() {
-//                 $this.text(this.countNum);
-//                 //alert('finished');
-//                 }
-            
-//             });  
+             });  
         
-        
-//         });
+            });
 
-//     }
+        }
 
-
-
+});
 
 
 
-// });
 
 
-// });
+
+
+
+
+

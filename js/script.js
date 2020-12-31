66d59a2037cf9ba9b410f19b1e9245a8bb6a5bfa
abdelaziz bannani <abdelaziz.bannani@ttl,be>
2020-12-31 23:15:10 +0100

    nav bar still needs some tweaking

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // $(window).on("load", function() 
 // {
-// $(".loader .inner").fadeOut(500, function()
+// $(".loader .inner").fadeOut(50000000, function()
 //     {
 //     $(".loader").fadeOut(750);
 //     });
@@ -153,5 +153,47 @@ $("#filters a").click(function() {
 });
 
 
+// const nav = $("#navigation");
+// const navTop = nav.offset().top;
 
+ //$(window).on("scroll", stickyNavigation);
+
+// function stickyNavigation ()
+//     {
+//     var body = $("body");
+//     if($(window).scrollTop() >= navTop)
+//         {
+//         body.css("padding-top", nav.outerHeight() + "px");
+//         body.addClass("fixedNav");
+//         }
+//     else
+//         {
+//         body.css("padding-top", 0);
+//         body.removeClass("fixedNav");
+//         }
+//     }
+
+
+
+
+const nav = $("#navigation");
+const navTop = nav.offset().top;
+
+$(window).on("scroll", stickyNavigation);
+
+
+function stickyNavigation()
+    {
+        var body = $("body");
+    if ($(window).scrollTop() >= navTop) 
+    {
+        body.css("padding-top", nav.outerHeight() + "px");
+        $('body').addClass('fixedNav');
+    }
+    else 
+    {
+        body.css("padding-top", 0);
+        $('body').removeClass('fixedNav');
+    }
+    }
 
